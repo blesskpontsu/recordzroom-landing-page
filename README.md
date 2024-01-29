7f773a0ee2a44c669e1ddec1d99ea25826d60c4e
blesskpontsu <blesskpontsu@gmail.com>
2024-01-29 17:23:20 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Recordzroom Landing Page
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="">
+    <meta name="robots" content="index,follow">
+    <meta name="description" content="This is the landing page for RecordzRoom Service. Your online Electronic Records and archival service.">
+    <meta name="keywords" content="RecordzRoom, E-Records, Electronic Records, Electronic-Records, Records, Archival, Archives, Employee Management">
+</head>
+<body>
+    
+</body>
+</html>
